33d1c84e6f4e2e7a61596c03c212a748b4f1ea17
Stepan Popov <apsivov@gmail.com>
2024-10-11 18:52:00 +0300

    Обновлена документация: добавлены описание функций и история изменений

--- circle.py
+++ circle.py
@@ -3,8 +3,12 @@ import math
 
 def area(r):
     return math.pi * r * r
+    # Вычиляем площадь окружности. Например: def area(2):
+    # 4Pi
 
 
 def perimeter(r):
     return 2 * math.pi * r
+    # Вычисляем периметр окружности. Например: def perimeter(2):
+    #4Pi
 
