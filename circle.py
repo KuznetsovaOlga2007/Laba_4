4ffeccc3f256b7a696489fdf5eee1c6c17e95794
Kostyaas <140883801+Kostyaas@users.noreply.github.com>
2024-10-12 12:25:04 +0300

    Add files via upload

--- circle.py
+++ circle.py
@@ -1,10 +1,20 @@
 import math
 
+"""
+Функция для вычисления площади круга.
 
+Пример вызова:
+t = area(5, 10)
+"""
 def area(r):
     return math.pi * r * r
 
+"""
+Функция для вычисления периметр круга.
 
+Пример вызова:
+t = perimeter(5, 10)
+"""
 def perimeter(r):
     return 2 * math.pi * r
 
