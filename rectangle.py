6c0149cda4ac75765b39f6163ce9f25a1fcb7076
itmo-coder <465296@niuitmo.ru>
2024-10-13 18:50:25 +0300

    fix: comments in source code files and README.md

--- rectangle.py
+++ rectangle.py
@@ -1,13 +1,9 @@
-'''
-a, b (float):sides of rectangle
-return value (float): area of rectangle
-'''
+# a, b (float):sides of rectangle
+# return value (float): area of rectangle
 def rectangle_area(a, b): 
     return a * b
 
-'''
-a, b (float): sides of rectangle
-return value (float): perimeter of rectangle
-'''
+# a, b (float): sides of rectangle
+# return value (float): perimeter of rectangle
 def rectangle_perimeter(a, b): 
     return 2 * (a + b) 
--- triangle.py
+++ triangle.py
@@ -1,14 +1,10 @@
-'''
-a (float): base side of triangle
-h (float): height of triangle
-return value (float): area of triangle
-'''
+# a (float): base side of triangle
+# h (float): height of triangle
+#return value (float): area of triangle
 def triangle_area(a, h): 
     return a * h / 2
 
-'''
-a, b, c (float): is a sides of triangle
-return value (float): perimeter of triangle
-'''
+# a, b, c (float): is a sides of triangle
+# return value (float): perimeter of triangle
 def triangle_perimeter(a, b, c): 
     return a + b + c 
