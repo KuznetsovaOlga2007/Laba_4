28d8b5382d5776b51055b8a2a2ff369bf790d30c
Vadim Garifullin <garifullinvadim2631@gmail.com>
2024-10-14 06:21:10 +0300

    changes

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    """Считает площадь круга"""
     return math.pi * r * r
 
 
 def perimeter(r):
+    """Считает периметр круга"""
     return 2 * math.pi * r
 
--- square.py
+++ square.py
@@ -1,7 +1,9 @@
 
 def area(a):
+    """Считает площадь квадрата"""
     return a * a
 
 
 def perimeter(a):
+    """Считает периметр квадрата"""
     return 4 * a
