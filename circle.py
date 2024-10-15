7ac30db8653568216a3eaa1f208faf8a771af5ab
Konstantin Nebykov <ptichka816@mail.ru>
2024-10-15 14:15:36 +0300

    Добавлены комментарии ко всем функциям во всех файлах

--- circle.py
+++ circle.py
@@ -2,9 +2,20 @@ import math
 
 
 def area(r):
+    """
+    Получает на вход r, возвращает r в квадрате умноженное на pi.
+
+    При вызове функции area(5)
+    5 * 5 * pi = 25pi
+    """
     return math.pi * r * r
 
 
 def perimeter(r):
-    return 2 * math.pi * r
+    """
+    Получает на вход r, возвращает r умноженное на 2 и на pi.
 
+    При вызове функции area(5)
+    5 * 2 * pi = 10pi
+    """
+    return 2 * math.pi * r
