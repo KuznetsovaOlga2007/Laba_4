e185827ce13e37720903947a425fb8afae6c963f
BlackDalekk <bakaev_ivan@rambler.ru>
2024-10-22 23:41:18 +0300

    One more descriptions

--- circle.py
+++ circle.py
@@ -2,9 +2,26 @@ import math
 
 
 def area(r):
+    """
+    Сalculation area of circle
+
+    Args:
+        r: Radius
+
+    Returns:
+        float: Circle's area
+    """
     return math.pi * r * r
 
 
 def perimeter(r):
-    return 2 * math.pi * r
+    """
+    Сalculation area of circle
 
+    Args:
+        r: Radius
+
+    Returns:
+        float: Circle's perimeter
+    """
+    return 2 * math.pi * r
