4b5f27d3b760cbfaed383daa321a8b3a88197d81
Timofey Kurilin <82908875+BrazenFatJR@users.noreply.github.com>
2024-11-01 14:19:17 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,7 +1,11 @@
+# square.py
 
 def area(a):
+    if a <= 0:
+        raise ValueError("Side length must be a positive number.")
     return a * a
 
-
 def perimeter(a):
+    if a <= 0:
+        raise ValueError("Side length must be a positive number.")
     return 4 * a
