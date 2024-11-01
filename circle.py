ab1e33a91d4ac29697e254ebbc3bd247e2495f4c
1fiolet1 <fiolet228and228@gmail.com>
2024-11-01 05:39:05 +0300

    commented out the functions

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
-    return math.pi * r * r
+    #Функция принимает радиус окружности и возвращает ее площадь
+    return math.pi * r * r 
 
 
 def perimeter(r):
+    #Функция принимает радиус окружности и возвращает длину окружности
     return 2 * math.pi * r
 
