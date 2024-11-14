28e0d54cf7df19ebac5d50845f294faa0fd172ef
Nikita <467341@niuitmo.ru>
2024-11-14 19:32:06 +0300

    .

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -3,12 +3,12 @@ import pytest
 
 
 def test_triangle_area():
-    # Тест для корректного вычисления площади треугольника
+    
     assert area(3, 4, 5) == 6
     assert area(5, 5, 6) == pytest.approx(12, 0.1)
 
 
 def test_triangle_perimeter():
-    # Тест для корректного вычисления периметра треугольника
+
     assert perimeter(3, 4, 5) == 12
     assert perimeter(5, 5, 6) == 16
