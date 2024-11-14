c15d661dab2ed5baf41144a3c2727200fc05ed50
Nikita <467341@niuitmo.ru>
2024-11-14 19:16:42 +0300

    .

--- calculate.py
+++ calculate.py
@@ -1,14 +1,33 @@
-figs = ['circle', 'square']
-funcs = ['perimeter', 'area']
-sizes = {}
+from circle import area as circle_area, perimeter as circle_perimeter
+from square import area as square_area, perimeter as square_perimeter
+from triangle import area as triangle_area, perimeter as triangle_perimeter
 
+figs = ['circle', 'square', 'triangle']
+funcs = ['perimeter', 'area']
+
+figures = {
+    'circle': {'area': circle_area, 'perimeter': circle_perimeter},
+    'square': {'area': square_area, 'perimeter': square_perimeter},
+    'triangle': {'area': triangle_area, 'perimeter': triangle_perimeter}
+}
+
+sizes = {
+    'circle-area': 1,
+    'circle-perimeter': 1,
+    'square-area': 1,
+    'square-perimeter': 1,
+    'triangle-area': 3,
+    'triangle-perimeter': 3
+}
 
 def calc(fig, func, size):
     assert fig in figs, f"Unknown figure: {fig}"
     assert func in funcs, f"Unknown function: {func}"
 
-    return eval(f'{fig}.{func}(*{size})')
+    key = f'{fig}-{func}'
+    assert len(size) == sizes[key], f"Incorrect number of arguments for {fig} {func}"
 
+    return figures[fig][func](size)
 
 if __name__ == "__main__":
     func = ''
@@ -25,9 +44,7 @@ if __name__ == "__main__":
         size = list(
             map(
                 int,
-                input(
-                    "Input figure sizes separated by space\n"
-                ).split()
+                input("Input figure sizes separated by space\n").split()
             )
         )
 
