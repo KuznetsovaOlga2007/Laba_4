6295822e1471081b921bed6931651c2176ac2f5d
Nikita <467341@niuitmo.ru>
2024-11-14 19:29:53 +0300

    .

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -3,7 +3,6 @@ import os
 import pytest
 from calculate import calc
 
-
 sys.path.insert(
     0, 
     os.path.abspath(
@@ -11,23 +10,18 @@ sys.path.insert(
     )
 )
 
-
 def test_calc_circle_area():
     assert calc('circle', 'area', [3]) == pytest.approx(28.27, 0.01)
-
 
 def test_calc_square_perimeter():
     assert calc('square', 'perimeter', [5]) == 20
 
-
 def test_calc_triangle_area():
     assert calc('triangle', 'area', [3, 4, 5]) == 6
-
 
 def test_calc_invalid_figure():
     with pytest.raises(AssertionError, match="Unknown figure: hexagon"):
         calc('hexagon', 'area', [5])
-
 
 def test_calc_invalid_function():
     with pytest.raises(AssertionError, match="Unknown function: volume"):
