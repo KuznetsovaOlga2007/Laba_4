4e35235fb95603355c6ffe11bcb3ba4b1567489d
Timofey Kurilin <82908875+BrazenFatJR@users.noreply.github.com>
2024-11-15 22:14:33 +0300

    Update test_triangle.py

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -1,10 +1,13 @@
-
 import pytest
 from triangle import perimeter
+
 
 class TestTriangle:
     def test_invalid_sides_perimeter(self):
         a, b, c = 1, 2, 3  # Does not satisfy the triangle inequality
         with pytest.raises(ValueError) as exc_info:
             perimeter(a, b, c)
-        assert "The sum of any two sides must be greater than the third side." in str(exc_info.value)
+        assert (
+            "The sum of any two sides must be greater than the third side."
+            in str(exc_info.value)
+        )
