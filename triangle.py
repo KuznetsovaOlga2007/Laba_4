a6008223fce59c919e7524eb3982530ac348aa91
Timofey Kurilin <82908875+BrazenFatJR@users.noreply.github.com>
2024-11-15 16:47:26 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -4,8 +4,10 @@ import math
 def area(a, b, c):
     if a <= 0 or b <= 0 or c <= 0:
         raise ValueError("All sides must be positive.")
-    if a + b <= c or a + c <= b or b + c <= a:
-        raise ValueError("The sum of any two sides must be greater than the third side.")
+    if (a + b <= c) or (a + c <= b) or (b + c <= a):
+        raise ValueError(
+            "The sum of any two sides must be greater than the third side."
+        )
     s = (a + b + c) / 2
     return math.sqrt(s * (s - a) * (s - b) * (s - c))
 
@@ -13,6 +15,8 @@ def area(a, b, c):
 def perimeter(a, b, c):
     if a <= 0 or b <= 0 or c <= 0:
         raise ValueError("All sides must be positive.")
-    if a + b <= c or a + c <= b or b + c <= a:
-        raise ValueError("The sum of any two sides must be greater than the third side.")
+    if (a + b <= c) or (a + c <= b) or (b + c <= a):
+        raise ValueError(
+            "The sum of any two sides must be greater than the third side."
+        )
     return a + b + c
