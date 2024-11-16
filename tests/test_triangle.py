a8e9619aa1d856205e2f6a56615e1f49523aa5cc
Yana <467976@niuitmo.ru>
2024-11-16 19:30:40 +0300

    Update test_triangle.py

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -3,7 +3,7 @@ import os
 import unittest
 from triangle import area, perimeter
 
-# Убедитесь, что отступы правильные
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 class TestTriangle(unittest.TestCase):
