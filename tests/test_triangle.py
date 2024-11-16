d65112d8b233d1a701cf07413437229fb00f1fbc
Yana <467976@niuitmo.ru>
2024-11-16 19:22:24 +0300

    Update test_triangle.py

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -9,33 +9,33 @@ sys.path.insert(0, os.path.\
 class TestTriangle(unittest.TestCase):
 
 
-def test_area(self):
-    self.assertEqual(area(3, 4, 5), 6)
+  def test_area(self):
+      self.assertEqual(area(3, 4, 5), 6)
 
 
-def test_perimeter(self):
-    self.assertEqual(perimeter(3, 4, 5), 12)
+  def test_perimeter(self):
+      self.assertEqual(perimeter(3, 4, 5), 12)
 
 
-def test_area_invalid(self):
-    with self.assertRaises(TypeError):
-        area(3, 4, "five")
+  def test_area_invalid(self):
+      with self.assertRaises(TypeError):
+          area(3, 4, "five")
 
 
-def test_perimeter_invalid(self):
-    with self.assertRaises(TypeError):
-        perimeter(3, 4, "five")
+  def test_perimeter_invalid(self):
+      with self.assertRaises(TypeError):
+          perimeter(3, 4, "five")
 
 
-def test_invalid_size_area(self):
-    with self.assertRaises(TypeError):
-        area(3, 4)
+  def test_invalid_size_area(self):
+      with self.assertRaises(TypeError):
+          area(3, 4)
 
 
-def test_invalid_size_perimeter(self):
-    with self.assertRaises(TypeError):
-        perimeter(3, 4)
+  def test_invalid_size_perimeter(self):
+      with self.assertRaises(TypeError):
+          perimeter(3, 4)
 
 
-if __name__ == '__main__':
-    unittest.main()
+  if __name__ == '__main__':
+      unittest.main()
