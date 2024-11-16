1f6cefae2ec5ad38b77795f72ae1e465ea3bd455
Yana <467976@niuitmo.ru>
2024-11-17 01:32:37 +0300

    Update test_calculate.py

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import pytest
-from calculate import calc, circle, square, triangle
+from calculate import calc, circle, square, triangle  # noqa: F401
 
 
 sys.path.append(
@@ -57,16 +57,18 @@ def test_invalid_function():
 
 
 def test_invalid_size_circle():
-    with pytest.raises(AssertionError, match="Invalid number of parameters for "
-                                              "area of circle"):
+    with pytest.raises(AssertionError, match=(
+        "Invalid number of parameters for area of circle")):
         calc('circle', 'area', [5, 52])
 
+
 def test_invalid_size_triangle():
-    with pytest.raises(AssertionError, match="Invalid number of parameters for "
-                                              "area of triangle"):
+    with pytest.raises(AssertionError, match=(
+        "Invalid number of parameters for area of triangle")):
         calc('triangle', 'area', [1, 52])
 
+
 def test_invalid_size_square():
-    with pytest.raises(AssertionError, match="Invalid number of parameters for "
-                                              "area of circle"):
+    with pytest.raises(AssertionError, match=(
+        "Invalid number of parameters for area of circle")):
         calc('circle', 'area', [3, 52])
