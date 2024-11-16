172a3043bfa8235464f8046fdbe59a5cd8741677
Yana <467976@niuitmo.ru>
2024-11-16 19:26:42 +0300

    Update test_square.py

--- tests/test_square.py
+++ tests/test_square.py
@@ -1,32 +1,29 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pytest
 from square import area, perimeter
 
+# Добавление пути к модулям
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def test_area():
     result = area(3)
     expected_result = 3 ** 2
     assert result == expected_result
 
-
 def test_perimeter():
     result = perimeter(3)
     expected_result = 3 * 4
     assert result == expected_result
 
-
 def test_area_invalid():
     with pytest.raises(TypeError):
         area("three")
-
 
 def test_invalid_size_area():
     with pytest.raises(TypeError):
         area(1, 52)
 
-        
 def test_invalid_size_perimeter():
     with pytest.raises(TypeError):
         perimeter(1, 52)
