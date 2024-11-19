0087062c2f45070a06c4f2baec3fb0d3fafd3f64
Vyacheslav Mukhin <466808@niuitmo.ru>
2024-11-19 22:01:01 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,18 +1,19 @@
-def area(a, b, c):
+def area(a: float, b: float, c: float) -> float:
+    # Проверка на отрицательные или нулевые длины сторон
     if a <= 0 or b <= 0 or c <= 0:
         raise ValueError("Side lengths must be positive numbers")
-    
+    # Проверка, чтобы стороны могли образовать треугольник
     if a + b <= c or a + c <= b or b + c <= a:
         raise ValueError("The provided sides do not form a triangle")
     
     return (a + b + c) / 2
 
-
-def perimeter(a, b, c):
+def perimeter(a: float, b: float, c: float) -> float:
+    # Проверка на отрицательные или нулевые длины сторон
     if a <= 0 or b <= 0 or c <= 0:
         raise ValueError("Side lengths must be positive numbers")
-    
+    # Проверка, чтобы стороны могли образовать треугольник
     if a + b <= c or a + c <= b or b + c <= a:
         raise ValueError("The provided sides do not form a triangle")
-    
+
     return a + b + c
