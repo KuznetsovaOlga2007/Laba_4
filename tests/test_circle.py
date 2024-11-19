70ec5ff6c5bbf11c71b21c6e1472804a9805ef76
Vyacheslav Mukhin <466808@niuitmo.ru>
2024-11-19 21:44:05 +0300

    Update test_circle.py

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -2,17 +2,17 @@ import unittest
 from circle import area, perimeter
 import math
 
-class TestCircle(unittest.TestCase):
 
+class TestCircle(unittest.TestCase):
     # Положительные тесты для площади круга
     def test_circle_area(self):
         # Arrange
         radius = 3
         expected_area = math.pi * radius * radius  # π * 3^2
-        
+
         # Act
         result = area(radius)
-        
+
         # Assert
         self.assertAlmostEqual(result, expected_area)
 
@@ -22,7 +22,7 @@ class TestCircle(unittest.TestCase):
 
         # Act
         result = area(radius)
-        
+
         # Assert
         self.assertAlmostEqual(result, expected_area)
 
@@ -31,20 +31,20 @@ class TestCircle(unittest.TestCase):
         # Arrange
         radius = 3
         expected_perimeter = 2 * math.pi * radius  # 2π * 3
-        
+
         # Act
         result = perimeter(radius)
-        
+
         # Assert
         self.assertAlmostEqual(result, expected_perimeter)
 
         # Arrange
         radius = 5
         expected_perimeter = 2 * math.pi * radius  # 2π * 5
-        
+
         # Act
         result = perimeter(radius)
-        
+
         # Assert
         self.assertAlmostEqual(result, expected_perimeter)
 
@@ -52,7 +52,7 @@ class TestCircle(unittest.TestCase):
     def test_zero_circle(self):
         # Arrange
         radius = 0
-        
+
         # Act and Assert
         with self.assertRaises(ValueError):
             area(radius)
@@ -63,12 +63,13 @@ class TestCircle(unittest.TestCase):
     def test_negative_circle(self):
         # Arrange
         radius = -3
-        
+
         # Act and Assert
         with self.assertRaises(ValueError):
             area(radius)
         with self.assertRaises(ValueError):
             perimeter(radius)
+
 
 if __name__ == '__main__':
     unittest.main()
