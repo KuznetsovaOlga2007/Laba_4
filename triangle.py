410e5d09047f11cb2577be0d39f0791f35b5d239
Vyacheslav Mukhin <466808@niuitmo.ru>
2024-11-19 21:48:01 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,6 +1,18 @@
 def area(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        raise ValueError("Side lengths must be positive numbers")
+    
+    if a + b <= c or a + c <= b or b + c <= a:
+        raise ValueError("The provided sides do not form a triangle")
+    
     return (a + b + c) / 2
 
 
 def perimeter(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        raise ValueError("Side lengths must be positive numbers")
+    
+    if a + b <= c or a + c <= b or b + c <= a:
+        raise ValueError("The provided sides do not form a triangle")
+    
     return a + b + c
