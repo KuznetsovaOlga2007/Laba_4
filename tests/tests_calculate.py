6fdcbc511dabbf9e11414c1fa896679ca7b5db60
dadayadada <ivanderagin30@gmail.com>
2024-11-21 00:18:32 +0300

    Update tests_calculate.py

--- tests/tests_calculate.py
+++ tests/tests_calculate.py
@@ -4,35 +4,37 @@ import circle
 import square
 from calculate import calc
 
+
 class TestCalc(unittest.TestCase):
 
     def test_circle_perimeter(self):
-        result = calc('circle', 'perimeter', [5])
+        result = calc("circle", "perimeter", [5])
         self.assertAlmostEqual(result, 2 * math.pi * 5)
 
     def test_circle_area(self):
-        result = calc('circle', 'area', [5])
+        result = calc("circle", "area", [5])
         self.assertAlmostEqual(result, math.pi * 5 * 5)
 
     def test_square_perimeter(self):
-        result = calc('square', 'perimeter', [5])
+        result = calc("square", "perimeter", [5])
         self.assertEqual(result, 4 * 5)
 
     def test_square_area(self):
-        result = calc('square', 'area', [5])
+        result = calc("square", "area", [5])
         self.assertEqual(result, 5 * 5)
 
     def test_invalid_figure(self):
         with self.assertRaises(AssertionError):
-            calc('triangle', 'perimeter', [5])
+            calc("triangle", "perimeter", [5])
 
     def test_invalid_function(self):
         with self.assertRaises(AssertionError):
-            calc('circle', 'volume', [5])
+            calc("circle", "volume", [5])
 
     def test_invalid_size(self):
         with self.assertRaises(TypeError):
-            calc('circle', 'perimeter', [5, 10])
+            calc("circle", "perimeter", [5, 10])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
