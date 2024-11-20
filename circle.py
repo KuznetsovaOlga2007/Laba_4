4580dfc059cf6e5a8e7ce25905d9417ef0991aff
dadayadada <ivanderagin30@gmail.com>
2024-11-20 20:57:05 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -2,11 +2,9 @@ import math
 
 
 def area(r):
-'''принимает число r,возращает площадь оркудности радиуса r'''
     return math.pi * r * r
 
 
 def perimeter(r):
-'''принимает число r,возращает перимитр круга радиуса r'''
     return 2 * math.pi * r
 
