b35a4b008f4b60db0b2f127074db7cd68bae0159
Vyacheslav Mukhin <466808@niuitmo.ru>
2024-11-20 17:16:36 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -5,8 +5,9 @@ def area(a: float, b: float, c: float) -> float:
     # Проверка, чтобы стороны могли образовать треугольник
     if a + b <= c or a + c <= b or b + c <= a:
         raise ValueError("The provided sides do not form a triangle")
-    
+
     return (a + b + c) / 2
+
 
 def perimeter(a: float, b: float, c: float) -> float:
     # Проверка на отрицательные или нулевые длины сторон
