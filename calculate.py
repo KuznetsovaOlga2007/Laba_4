a7f9bd237a19f405e5b0f17156ee53dd856ab51f
dadayadada <ivanderagin30@gmail.com>
2024-11-20 20:56:22 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,15 +1,10 @@
 import circle
 import square
 
-'''определение доступных фигур и функций '''
 figs = ['circle', 'square']
 funcs = ['perimeter', 'area']
 sizes = {}
 
-'''calc принимает три аргумента: fig (фигура), func (функция) и size (размеры).'''
-'''Проверка корректности ввода: Используются утверждения (assert), чтобы убедиться, что вводимые фигуры есть в списках'''
-'''вычисление результата'''
-'''Вывод результата'''
 def calc(fig, func, size):
     assert fig in figs
     assert func in funcs
@@ -17,23 +12,19 @@ def calc(fig, func, size):
     result = eval(f'{fig}.{func}(*{size})')
     return result
 
-'''устанавливаются пустые значения, в будущем которые будут заполнены пользователем'''
 if __name__ == "__main__":
     func = ''
     fig = ''
     size = list()
 
-    '''запрашивается у пользователя ввести название фигуры. Цикл будет повторяться, пока не будет введено корректное название из списка figs.'''
     while fig not in figs:
         fig = input(f"Enter figure name, available are {figs}:\n")
 
-    '''ввод размеров'''
     while func not in funcs:
         func = input(f"Enter function name, available are {funcs}:\n")
 
     while len(size) != sizes.get(f"{func}-{fig}", 1):
         size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
 
-    '''вызов функции'''
     result = calc(fig, func, size)
     print(f'{func} of {fig} is {result}')
