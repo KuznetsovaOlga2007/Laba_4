a2f09b71b95ad38b0df4f582488d29e49cb1b5d6
20moonlight01 <nadezdaglebova06@gmail.com>
2024-11-20 18:55:36 +0300

    new test file on square added

---
+++ square_test.py
@@ -0,0 +1,24 @@
+import unittest
+import square
+
+
+class SquareTestCase(unittest.TestCase):
+    def test_zero_mul(self):
+        res = square.area(0)
+
+        self.assertEqual(res, 0)
+
+    def test_mul(self):
+        res = square.area(10)
+
+        self.assertEqual(res, 100)
+
+    def test_zero_sum(self):
+        res = square.perimeter(0)
+
+        self.assertEqual(res, 0)
+
+    def test_sum(self):
+        res = square.perimeter(10)
+
+        self.assertEqual(res, 40)
