dcd03cc89f1463d9c76055eabb61b7a3617e8219
Kirill Lukashenko <466542@niuitmo.ru>
2024-11-21 20:26:10 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,7 +1,10 @@
-
 def area(a):
+    if a < 0:
+        raise AssertionError("mistake")
     return a * a
 
 
 def perimeter(a):
+    if a < 0:
+        raise AssertionError("mistake")
     return 4 * a
