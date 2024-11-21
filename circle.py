6ac98fc088e5b61ddff846dcf195c7abcbde40ca
Kirill Lukashenko <466542@niuitmo.ru>
2024-11-21 20:25:14 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,12 @@ import math
 
 
 def area(r):
+    if r < 0:
+        raise AssertionError("mistake")
     return math.pi * r * r
 
 
 def perimeter(r):
+    if r < 0:
+        raise AssertionError("mistake")
     return 2 * math.pi * r
-
