82847a4456e78ad638816a3acd4dd434e9dad641
Nursafin Karim <103069526+Karim-n-007@users.noreply.github.com>
2024-11-21 21:32:40 +0300

    update file

--- test_rectangle.py
+++ test_rectangle.py
@@ -5,7 +5,7 @@ from rectangle import perimeter
 class TestRectangleFunctions(unittest.TestCase):
     def test_area_positive_integers(self):
         # Тестирует функцию area с положительными целыми числами для сторон прямоугольника
-        self.assertEqual(area(5, 10), 50)  
+        self.assertEqual(area(8, 4), 32)  
 
     def test_area_zero(self):
         # Тестирует функцию area при нулевом значении одной из сторон
@@ -17,12 +17,11 @@ class TestRectangleFunctions(unittest.TestCase):
 
     def test_area_positive_floats(self):
         # Тестирует функцию area с положительными числами с плавающей точкой
-        self.assertEqual(area(2.5, 4.0), 10.0)  
-
+        self.assertEqual(area(7.5, 3.5), 26.25)  
 
     def test_perimeter_positive_integers(self):
         # Тестирует функцию perimeter с положительными целыми числами для сторон
-        self.assertEqual(perimeter(5, 10), 30) 
+        self.assertEqual(perimeter(8, 4), 24) 
 
     def test_perimeter_zero(self):
         # Тестирует функцию perimeter при нулевом значении одной из сторон
@@ -34,7 +33,7 @@ class TestRectangleFunctions(unittest.TestCase):
 
     def test_perimeter_positive_floats(self):
         # Тестирует функцию perimeter с положительными числами с плавающей точкой
-        self.assertEqual(perimeter(2.5, 4.0), 13.0)  
+        self.assertEqual(perimeter(7.5, 3.5), 22.0)  
 
 if __name__ == '__main__':
     unittest.main()
