86f26b63e74e62c73ad26534bcf42a9c2636f69d
Kirill Lukashenko <466542@niuitmo.ru>
2024-11-22 13:24:58 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -3,11 +3,11 @@ import math
 
 def area(r):
     if r < 0:
-        raise AssertionError("mistake")
+        raise ValueError("Radius cannot be negative")
     return math.pi * r * r
 
 
 def perimeter(r):
     if r < 0:
-        raise AssertionError("mistake")
+        raise ValueError("Radius cannot be negative")
     return 2 * math.pi * r
