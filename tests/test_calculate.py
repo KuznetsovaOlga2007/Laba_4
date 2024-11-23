359574dd8973192ab963b5dce612c98075813492
Vyacheslav Mukhin <466808@niuitmo.ru>
2024-11-23 20:39:05 +0300

    Update test_calculate.py

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -1,46 +1,52 @@
 import unittest
-import circle
-import square
+from main import calc
 
 
 class TestCalculate(unittest.TestCase):
     def test_valid_square_area(self):
-        # Проверка корректного вычисления площади квадрата
         self.assertEqual(calc("square", "area", [4]), 16)
 
     def test_valid_square_perimeter(self):
-        # Проверка корректного вычисления периметра квадрата
         self.assertEqual(calc("square", "perimeter", [4]), 16)
 
     def test_valid_circle_area(self):
-        # Проверка корректного вычисления площади круга
         self.assertAlmostEqual(calc("circle", "area", [3]), 28.274333882308138)
 
     def test_valid_circle_perimeter(self):
-        # Проверка корректного вычисления периметра круга
         self.assertAlmostEqual(calc("circle", "perimeter", [3]), 18.84955592153876)
 
     def test_invalid_figure(self):
-        # Проверка некорректной фигуры
         with self.assertRaises(AssertionError):
             calc("triangle", "area", [3])
 
     def test_invalid_function(self):
-        # Проверка некорректной функции
         with self.assertRaises(AssertionError):
             calc("circle", "volume", [3])
 
     def test_invalid_size_length_square(self):
-        # Проверка некорректного числа параметров для квадрата
         with self.assertRaises(ValueError):
             calc("square", "area", [4, 5])
 
     def test_invalid_size_length_circle(self):
-        # Проверка некорректного числа параметров для круга
         with self.assertRaises(ValueError):
             calc("circle", "perimeter", [3, 5])
 
+    def test_negative_size_square(self):
+        with self.assertRaises(ValueError):
+            calc("square", "area", [-4])
 
-# Запуск тестов
+    def test_zero_size_square(self):
+        with self.assertRaises(ValueError):
+            calc("square", "perimeter", [0])
+
+    def test_negative_size_circle(self):
+        with self.assertRaises(ValueError):
+            calc("circle", "area", [-3])
+
+    def test_zero_size_circle(self):
+        with self.assertRaises(ValueError):
+            calc("circle", "perimeter", [0])
+
+
 if __name__ == "__main__":
     unittest.main()
