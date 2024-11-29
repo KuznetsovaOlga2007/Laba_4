0f559d3398c35ec729440d2e38feb95e413a5d77
k1fu <vanokross@ya.r>
2024-11-29 14:45:08 +0300

    fix 13 ver 2.32

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -1,17 +1,21 @@
 import pytest
 import re
-from calculate import calc  # Замените на имя вашего файла
+from calculate import calc
 
-# Проверка на ошибку, если переданы некорректные данные
-@pytest.mark.parametrize("fig, func, size, exception, message", [
-    ('circle', 'area', [0], ValueError, "Parameters must be positive numbers"),
-    ('square', 'perimeter', [-1], ValueError, "Parameters must be positive numbers"),
-    ('triangle', 'area', [3, 4], ValueError, "Wrong parameters. Expected 3,got 2"),
-    ('square', 'area', ['a'], TypeError, "Parameters must be numbers"),
-])
+
+@pytest.mark.parametrize(
+    "fig, func, size, exception, message", [
+        ('circle', 'area', [0], ValueError,
+         "Parameters must be positive numbers"),
+        ('square', 'perimeter', [-1], ValueError,
+         "Parameters must be positive numbers"),
+        ('triangle', 'area', [3, 4], ValueError,
+         "Wrong parameters. Expected 3, got 2"),
+        ('square', 'area', ['a'], TypeError,
+         "Parameters must be numbers"),
+    ]
+)
 def test_invalid_calculations(fig, func, size, exception, message):
     with pytest.raises(exception) as excinfo:
         calc(fig, func, size)
-    
-    # Проверяем, что сообщение об ошибке содержит ожидаемую строку, игнорируя пробелы
     assert re.search(message, str(excinfo.value), re.DOTALL) is not None
