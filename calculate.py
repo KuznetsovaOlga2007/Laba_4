8d3c7ef2c7847d313b8c509a7e1a9a50e7d1cb7d
k1fu <vanokross@ya.r>
2024-11-30 15:40:01 +0300

    fix13 ver 2.5

--- calculate.py
+++ calculate.py
@@ -20,3 +20,19 @@ def calc(fig, func, size):
 
     result = eval(f"{fig}.{func}(*{size})")
     return result
+
+if __name__ == "__main__":
+	func = ''
+	fig = ''
+	size = list()
+
+	while fig not in figs:
+		fig = input(f"Enter figure name, avaliable are {figs}:\n")
+
+	while func not in funcs:
+		func = input(f"Enter function name, avaliable are {funcs}:\n")
+
+	while len(size) != sizes.get(f"{func}-{fig}", 1):
+		size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
+
+	calc(fig, func, size)
