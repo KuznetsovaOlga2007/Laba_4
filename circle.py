95986d4d46f43504127ab270357f9a7faf6dc91e
Impathy <fa_1d@bk.ru>
2024-12-20 22:54:49 +0300

    Revert "added tests"
    This reverts commit 374cb0b629412ab0b4acb2715803fc20054979d2.

--- circle.py
+++ circle.py
@@ -1,17 +1,12 @@
 import math
 
-def circleArea(r):
-    '''Принимает 1 значение r. r - радиус окружности. Возвращает площадь окружности.'''
-    if not isinstance(r, (int, float)):
-        raise TypeError("Радиус должен быть числом.")
-    if r < 0:
-        raise ValueError("Радиус не может быть отрицательным.")
+
+def area(r):
+    '''Принимает радиус окружности - r. Возвращает площадь окружности.'''
     return math.pi * r * r
 
-def circlePerimeter(r):
-    '''Принимает 1 значение r. r - радиус окружности. Возвращает периметр окружности.'''
-    if not isinstance(r, (int, float)):
-        raise TypeError("Радиус должен быть числом.")
-    if r < 0:
-        raise ValueError("Радиус не может быть отрицательным.")
-    return math.pi * r * 2
+
+def perimeter(r):
+    '''Принимает радиус окружности - r. Возвращает длинну окружности.'''
+    return 2 * math.pi * r
+
--- triangle.py
+++ triangle.py
@@ -1,15 +1,8 @@
-def traingleArea(a, h):
-    '''Принимает 2 значения, a и h. a - сторона треугольника, h - высота, проведенная к стороне a. Возвращает площадь треугольника.'''
-    if not isinstance(a, (int, float)) or not isinstance(h, (int, float)):
-        raise TypeError("Сторона и высота должны быть числами.")
-    if a < 0 or h < 0:
-        raise ValueError("Сторона и высота не могут быть отрицательными.")
+def area(a, h):
+    '''Принимает сторону треугольника и высоту проведенную к этой стороне - a, h. Возвращает площадь треугольника.'''
     return a * h / 2
 
-def trainglePerimeter(a, b, c):
-    '''Принимает 3 значения, a, b и c. a - первая сторона треугольника, b - вторая сторона треугольника, c - третья сторона треугольника. Возвращает периметр треугольника.'''
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)) or not isinstance(c, (int, float)):
-        raise TypeError("Все стороны должны быть числами.")
-    if a < 0 or b < 0 or c < 0:
-        raise ValueError("Стороны не могут быть отрицательными.")
+
+def perimeter(a, b, c):
+    '''Принимает 3 стороны треугольника - a, b, c. Возвращает периметр треугольника.'''
     return a + b + c
