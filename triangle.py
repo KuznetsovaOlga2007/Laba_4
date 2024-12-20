b42eeb76589dc6ff477fea9f68af153f6fd98e2a
Pasha1 <vahtin.pasha@yandex.ru>
2024-12-20 07:53:15 +0300

    change

---
+++ triangle.py
@@ -0,0 +1,10 @@
+def area(a, b, c):  # Площадь указана некорректно
+    # Замена на формулу Герона
+    # return (a + b + c) / 2
+    p = (a + b + c) / 2
+    S = p * (p - a) * (p - b) * (p - c)
+    return S ** 0.5
+
+
+def perimeter(a, b, c):
+    return a + b + c
